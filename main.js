f053e67dacd30d3df4509ca54bd071d99f3f8eaf
cyberstalke <abduvaliyeabdulaziz@gmail.com>
2023-01-25 23:02:03 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// alert('Hi cool')
+
+// const place = confirm('hi');
+// console.log = (place);
+// const place = prompt("Hi", "")
+
+// console.log(place)
+
+
+
+// const color = [];
+// color[0] = prompt('What is your favourite color', '');
+// color[1] = prompt('What is your favourite color', '');
+// color[2] = prompt('What is your favourite color', '');
+
+
+// console.log(color); 
+
+
+// const place = confirm('Areu meried');
+
+// console.log(place);
+
+
+// const place = prompt("What is your name", "")
+
+// console.log(place);
+
+
+
+
+// const job = prompt('Where fo u work', '');
+// const userName = prompt('NAme', '');
+
+
+// console.log(`Name is ${userName},Job is ${job}`);
+
+
+// const age = true;
+// const Lastname = true;
+// const Color = false;
+
+// console.log(!age || !Lastname || Color);
